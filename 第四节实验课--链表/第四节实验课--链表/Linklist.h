ba7cefdb4672387b90c319218f5adc616419ab89
俊安 周 <zhoucookie@outlook.com>
2022-04-06 14:59:50 +0800

    更新

--- 第四节实验课--链表/第四节实验课--链表/Linklist.h
+++ 第四节实验课--链表/第四节实验课--链表/Linklist.h
@@ -5,13 +5,13 @@ typedef struct Linklist {
 	struct Linklist* next;
 }SqList;
 
-void InitList_Sq(Linklist*& L);//初始化链表
-bool ListInsert_Sq(Linklist*& L, int i, ElemType e);//插入元素
-bool ListEmpty_Sq(Linklist* L);//判断链表是否为空
-int ListLength_Sq(Linklist* L);//计算链表长度
-int LocateElem_Sq(Linklist* L, ElemType e);//返回 L中第一个其值与e相等的元素的位序
-bool ListDelete_Sq(Linklist*& L, int i, ElemType& e);//删除元素
-void DestroyList_Sq(Linklist*& L);//销毁链表L，释放链表L占用的内存空间
-bool Listlocate_Sq(Linklist*& L, int i, ElemType& e);//输出链表的第i个元素
-void CreatList_Sq(Linklist*& L, ElemType a[], int n);//尾插法创建链表
-void deletes(Linklist*& L, char min, char max);//删除min和max之间的元素
+void InitList_Sq(Linklist*& L);
+bool ListInsert_Sq(Linklist*& L, int i, ElemType e);
+bool ListEmpty_Sq(Linklist* L);
+int ListLength_Sq(Linklist* L);
+int LocateElem_Sq(Linklist* L, ElemType e);
+bool ListDelete_Sq(Linklist*& L, int i, ElemType& e);
+void DestroyList_Sq(Linklist*& L);
+bool Listlocate_Sq(Linklist*& L, int i, ElemType& e);
+void CreatList_Sq(Linklist*& L, ElemType a[], int n);
+void deletes(Linklist*& L, char min, char max);
--- 第四节实验课--链表/第四节实验课--链表/main.cpp
+++ 第四节实验课--链表/第四节实验课--链表/main.cpp
@@ -1,3 +1,15 @@
+////（1）初始化单链表h
+////（2）采用尾插法依次插入a、b、c、d、e
+////（3）输出单链表h
+////（4）输出单链表h的长度
+////（5）判断单链表h是否为空
+////（6）输出单链表h的第3个元素
+////（7）输出元素a的逻辑位置
+////（8）在第4个元素位置上插入元素f
+////（9）输出单链表h
+////（10）删除单链表h的第3个元素
+////（11）输出单链表h
+////（12）释放单链表h
 //（1）使用实验2 - 1已完成的单链表的设计LinkList.cpp；
 //（2）按实验要求编写删除算法
 //算法提示 : deletes(LinkList * L, int min, int max).算法提示：从链表中找到第一个大于min的值，从该位置往后再找到第一个大于max的值，重新完成指针链接，从而实现删除。
@@ -38,44 +50,3 @@ int main()
 	DestroyList_Sq(L);
 	return 0;
 }
-
-//（1）初始化单链表h
-//（2）采用尾插法依次插入a、b、c、d、e
-//（3）输出单链表h
-//（4）输出单链表h的长度
-//（5）判断单链表h是否为空
-//（6）输出单链表h的第3个元素
-//（7）输出元素a的逻辑位置
-//（8）在第4个元素位置上插入元素f
-//（9）输出单链表h
-//（10）删除单链表h的第3个元素
-//（11）输出单链表h
-//（12）释放单链表h
-
-//int main()
-//{
-//	ElemType e;
-//	Linklist* L;
-//	char a[] = "abcde";
-//	printf("依次插入a、b、c、d、e\n");
-//	CreatList_Sq(L, a, 5);
-//	printf("输出顺序表L:");
-//	ListTraverse_Sq(L);
-//	printf("输出顺序表L的长度:%d\n", ListLength_Sq(L));
-//	printf("判断顺序表L是否为空(1表示为空/0表示非空):%d\n", ListEmpty_Sq(L));
-//	printf("输出顺序表的第3个元素:");
-//	Listlocate_Sq(L, 3, e);
-//	printf("%c\n", e);
-//	printf("输出元素a的逻辑位置:%d\n", LocateElem_Sq(L, 'a'));
-//	printf("在第4个元素位置上插入元素f\n");
-//	ListInsert_Sq(L, 4, 'f');
-//	printf("输出顺序表L:");
-//	ListTraverse_Sq(L);
-//	printf("删除L的第3个元素\n");
-//	ListDelete_Sq(L, 3, e);
-//	printf("输出顺序表L:");
-//	ListTraverse_Sq(L);
-//	printf("释放顺序表L");
-//	DestroyList_Sq(L);
-//	return 0;
-//}
